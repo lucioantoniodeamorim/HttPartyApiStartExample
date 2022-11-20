494a494fed429d91100109ebb76187fcdb4d1595
Lúcio Antônio de Amorim <118086005+lucioantoniodeamorim@users.noreply.github.com>
2022-11-20 02:29:38 -0300

    Create spec_helper.rb

---
+++ spec_helper.rb
@@ -0,0 +1,16 @@
+require 'httparty'
+require 'httparty/request'
+require 'httparty/response/headers'
+
+require_relative '../services/pokemons_service.rb'
+ 
+RSpec.configure do |config|
+  include Pokemon
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+ end
